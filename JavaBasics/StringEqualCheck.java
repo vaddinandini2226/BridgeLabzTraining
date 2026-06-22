ea54a461e88f94d9dbd4be3dec2fb713fd54c94a
vaddinandini2226 <vaddinandini2004@gmail.com>
2026-06-22 10:42:43 +0530

    program for checking both strings are equal or not

---
+++ JavaBasics/StringEqualCheck.java
@@ -0,0 +1,16 @@
+package basicsPgms;
+
+import java.util.Scanner;
+
+public class StringEqualCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine(); 
+			System.out.println(s1.equals(s2));
+			sc.close();
+	}
+
+}
