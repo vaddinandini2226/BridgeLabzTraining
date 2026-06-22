8c2af18a19608bba1f58505b76eb48365b944efc
vaddinandini2226 <vaddinandini2004@gmail.com>
2026-06-22 16:36:29 +0530

    checking vowel or consonant

---
+++ Basic-Core-Programs/VowelConsonant.java
@@ -0,0 +1,22 @@
+package basicsPgms;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an alphabet: ");
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            System.out.println(ch + " is a Vowel.");
+        else
+            System.out.println(ch + " is a Consonant.");
+
+        sc.close();
+    }
+}
