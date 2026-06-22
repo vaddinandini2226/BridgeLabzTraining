39a29d15b7d088a32e72a5d4853ca03b10b40244
vaddinandini2226 <vaddinandini2004@gmail.com>
2026-06-22 15:45:46 +0530

    snake and ladder game with three options until player reach 100

---
+++ SnakeAndLadder/SnakeAndLadderUC3.java
@@ -0,0 +1,68 @@
+package snakeandladder;
+
+import java.util.Random;
+
+public class SnakeAndLadderUC3 {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int position = 0;
+
+        System.out.println("Snake and Ladder Game Started");
+        System.out.println("Initial Position: " + position);
+
+        while (position != 100) {
+
+            // Roll the dice (1 to 6)
+            int dice = random.nextInt(6) + 1;
+
+            // Generate option
+            // 0 = No Play
+            // 1 = Ladder
+            // 2 = Snake
+            int option = random.nextInt(3);
+
+            System.out.println("\nDice Rolled: " + dice);
+
+            switch (option) {
+
+                case 0:
+                    System.out.println("Option: No Play");
+                    // Position remains the same
+                    break;
+
+                case 1:
+                    System.out.println("Option: Ladder");
+
+                    // Move only if position does not exceed 100
+                    if (position + dice <= 100) {
+                        position += dice;
+                    } else {
+                        System.out.println("Move exceeds 100. Stay at same position.");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Option: Snake");
+
+                    position -= dice;
+
+                    // If position goes below 0, restart from 0
+                    if (position < 0) {
+                        position = 0;
+                    }
+                    break;
+            }
+
+            System.out.println("Current Position: " + position);
+        }
+
+        System.out.println("\n******************************");
+        System.out.println("Congratulations!");
+        System.out.println("Player reached Position 100.");
+        System.out.println("Player Wins!");
+        System.out.println("******************************");
+    }
+}
